ef270f4c853953692dd4f421c7693fd05eb96eb6
James <james@rubify.com>
2014-06-29 23:24:54 +0800

    fix style

--- app/models/quiz_pool_question.rb
+++ app/models/quiz_pool_question.rb
@@ -4,4 +4,12 @@ class QuizPoolQuestion < ActiveRecord::Base
   has_many :correct_quiz_pool_question_options, -> { where(is_correct: true) }, class_name: "QuizPoolQuestionOption", foreign_key: "quiz_pool_question_id" 
   
   accepts_nested_attributes_for :quiz_pool_question_options, allow_destroy: true
+  
+  validate :at_least_one_correct_option
+  
+  def at_least_one_correct_option
+    if self.correct_quiz_pool_question_options.length == 0
+      self.errors.add(:quiz_pool_question_options, :at_least_one_correct_answer)
+    end
+  end
 end
